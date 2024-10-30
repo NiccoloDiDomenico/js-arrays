1f9bcd16ed3a9007ae9cb358a3b609e7f2609015
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-10-30 19:07:09 +0100

    feat: complete exercise 6

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -43,6 +43,9 @@ console.log(teachers[teachers.length -1]);
 
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah')
+console.log(teachers[0]);
+
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
